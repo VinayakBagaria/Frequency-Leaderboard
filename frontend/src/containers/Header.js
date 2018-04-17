9f2781fae022eba65701ae30d43434d670cd0215
VinayakBagaria <workvinayak95@gmail.com>
2018-04-17 19:20:26 +0530

    docker removed and clean style in a single file

--- frontend/src/containers/Header.js
+++ frontend/src/containers/Header.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const Header = () => <h1>Header</h1>;
+const Header = () => (
+  <div className="header">
+    <h1>Frequency</h1>
+    <h1>Dashboard</h1>
+  </div>
+);
 
 export default Header;
